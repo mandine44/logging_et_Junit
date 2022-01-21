956ed649c28c229609142e144b4ec2d2d96d241f
Amandine Huret <amandine.huret@gmail.com>
2022-01-21 15:36:37 +0100

    TP01commenté

--- src/main/java/dev/console/App.java
+++ src/main/java/dev/console/App.java
@@ -6,8 +6,12 @@ import dev.service.*;
 
 public class App {
 	
-	private static final Logger LOG = LoggerFactory.getLogger(App.class);
-
+	
+	private static final Logger LOG = LoggerFactory.getLogger(App.class); 
+	// App.class = nom du logger, = nom de la classe par convention, correspond au nom du logger dans fichier logback.xml
+	//permet de pouvoir afficher le nom de la classe qui a poussé le log dans le fichier de sortie ou la console
+	
+	
 	public static void main(String[] args) {
 		LOG.info("Bonjour Slf4J !");
 		LOG.info("Implementation Logback");
